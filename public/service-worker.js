d49eb78a47c90b5963fbde6c057be76930b8c22a
Melanie Bostwick <mbostwick1@gmail.com>
2021-01-13 21:22:34 -0500

    replaced service-worker.js with correct code

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,56 +1,72 @@
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/db.js",
-  "/index.js",
-  "/favicon.ico",
-  "/manifest.webmanifest",
-  "/service-worker.js",
+  "db.js",
+  "favicon.ico",
+  "index.js",
+  "manifest.webmanifest",
+  "service-worker.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
-  "/style.css"
+  "styles.css"
 ];
 
-const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
-self.addEventListener("install", function(evt) {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+self.addEventListener("install", function (evt) {
+evt.waitUntil(
+  caches
+    .open(CACHE_NAME)
+    .then((cache) => {
       console.log("Your files were pre-cached successfully!");
-      return cache.addAll(FILES_TO_CACHE);
+      cache
+        .addAll(FILES_TO_CACHE)
+        .then((result) => {
+          // debugger;
+          console.log("result of add all", result);
+        })
+        .catch((err) => {
+          // debugger;
+          console.log("Add all error: ", err);
+        });
     })
-  );
+    .catch((err) => {
+      console.log(err);
+    })
+);
 
-  self.skipWaiting();
+self.skipWaiting();
 });
 
-self.addEventListener("activate", function(evt) {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+// activate
+self.addEventListener("activate", function (evt) {
+evt.waitUntil(
+  caches.keys().then((keyList) => {
+    return Promise.all(
+      keyList.map((key) => {
+        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+          console.log("Removing old cache data", key);
+          return caches.delete(key);
+        }
+      })
+    );
+  })
+);
 
-  self.clients.claim();
+self.clients.claim();
 });
 
 // fetch
-self.addEventListener("fetch", function(evt) {
-  // cache successful requests to the API
-  if (evt.request.url.includes("/api/")) {
-    evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
+self.addEventListener("fetch", function (evt) {
+if (evt.request.url.includes("/api/")) {
+  evt.respondWith(
+    caches
+      .open(DATA_CACHE_NAME)
+      .then((cache) => {
         return fetch(evt.request)
-          .then(response => {
+          .then((response) => {
             // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
               cache.put(evt.request.url, response.clone());
@@ -58,22 +74,22 @@ self.addEventListener("fetch", function(evt) {
 
             return response;
           })
-          .catch(err => {
+          .catch((err) => {
             // Network request failed, try to get it from the cache.
             return cache.match(evt.request);
           });
-      }).catch(err => console.log(err))
-    );
-
-    return;
-  }
-
-  // if the request is not for the API, serve static assets using "offline-first" approach.
-  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
-  evt.respondWith(
-    caches.match(evt.request).then(function(response) {
-      return response || fetch(evt.request);
-    })
+      })
+      .catch((err) => console.log(err))
   );
-});
 
+  return;
+}
+
+evt.respondWith(
+  caches.open(CACHE_NAME).then((cache) => {
+    return cache.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    });
+  })
+);
+});
